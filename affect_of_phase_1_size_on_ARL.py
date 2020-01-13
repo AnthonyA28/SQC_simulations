44ddd40597b3e5437758c68b7d39f4485727ab84
Anthony <arrowood.aj@ufl.edu>
2020-01-13 01:38:49 -0500

    fixed the medthod of calculating the ARL to correspond approximately with that of Queensbery 1992 "The effect of Sample Size on Estimated Limits for X_bar and X Control Charts "

--- affect_of_phase_1_size_on_ARL.py
+++ affect_of_phase_1_size_on_ARL.py
@@ -26,35 +26,35 @@ def get_sample_stdDev(X):
         n = n + 1
     return (sumSqr/(n-1))**0.5
 
+def get_MR(X):
+    MR = np.zeros(X.size-1)
+    for i in range(X.size-1):
+        MR[i] = abs(X[i+1]- X[i])
+    assert(X.size - 1 == MR.size)
+    return MR
+
 
 """ Input parameters  """
-mean = 0 # the known mean
-stdDev = 5  # the known stdDev
-size_phase_2 = 10000
-sims = 50 # the amount of simulations to run
+sims = 10000 # the amount of simulations to run
 c = 3.00 # the spread of the control limits
-max_size_phase_1 = 5000
-min_size_phase_1 = 50
-skip = 10 # increment of phase_1 size
 
-print("size, ARL_0")
-for size_phase_1 in range(min_size_phase_1, max_size_phase_1, skip):
 
-    sumTotal_OCL = 0 # total number of points out of control limits
-    n = 0  # total number of  points
+for size in [30,50,75,100,200,300,500,1000,2000,10000]:
+    arl_sum = 0 
     for sim in range(sims):
+        rl      = 0 # The run length for this dataset
+        Xp1     = np.random.normal(0, 1, size)  # The phase 1 dataset
+        X_bar   = get_mean(Xp1)
+        MR      = get_MR(Xp1)
+        MR_bar  = get_mean(MR)
+        ucl     = X_bar + 3*MR_bar/1.128
+        lcl     = X_bar - 3*MR_bar/1.128
+        while True:
+            x   = np.random.normal(0, 1)
+            rl  += 1
+            if x > ucl or x < lcl:
+                break
+        arl_sum += rl
+    # print(arl_sum/sims)
+    print("size: ", size, ", Arl: ", arl_sum/sims)
 
-        X = np.random.normal(mean, stdDev, size_phase_1 + size_phase_2) #generate random normal data with known mean and stdDev
-        Xp1 = X[:size_phase_1]
-        Xp2 = X[size_phase_1:]
-        # mean_est = get_mean(Xp1) # estimate the mean with only phase 1 data
-        # stdDev_est = get_sample_std_dev(Xp1) # estimate the standard deviation with only phase 1 data
-        stdDev_est = get_sample_stdDev(Xp1)
-        mean_est = get_mean(Xp1)
-        ucl = mean_est+c*stdDev_est
-        lcl = mean_est-c*stdDev_est
-        assert(Xp2.size == size_phase_2)
-        assert(Xp1.size == size_phase_1)
-        n = n + size_phase_2
-        sumTotal_OCL = sumTotal_OCL + get_num_OCL(Xp2, ucl, lcl) # only include phase 2 data
-    print("%.4f,%.4f" % ( size_phase_1, n/sumTotal_OCL))
